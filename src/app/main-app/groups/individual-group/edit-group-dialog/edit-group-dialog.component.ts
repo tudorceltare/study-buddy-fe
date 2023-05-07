9a8eac71c11c6e2fc9f72bd61bc77fb99ebe80ca
tudorceltare <tudor.pitic@gmail.com>
2023-05-07 17:17:08 +0300

    Add update group Topics in edit dialog

--- src/app/main-app/groups/individual-group/edit-group-dialog/edit-group-dialog.component.ts
+++ src/app/main-app/groups/individual-group/edit-group-dialog/edit-group-dialog.component.ts
@@ -1,4 +1,5 @@
-import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
+import {Component, ElementRef, Inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {GroupsService} from "../../groups.service";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {NotificationService} from "../../../../service/notification.service";
@@ -6,6 +7,10 @@ import {NotificationType} from "../../../../models/notificationType.enum";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Group} from "../../../../models/group.model";
 import {HttpErrorResponse} from "@angular/common/http";
+import {Topic} from "../../../../models/topic.model";
+import {MatChipInputEvent} from "@angular/material/chips";
+import {MatAutocompleteSelectedEvent} from "@angular/material/autocomplete";
+import {map, Observable, startWith} from "rxjs";
 
 @Component({
   selector: 'app-edit-group-dialog',
@@ -19,9 +24,23 @@ export class EditGroupDialogComponent implements OnInit, OnDestroy {
     name: new FormControl('', [Validators.required]),
     description: new FormControl('', [Validators.required]),
     location: new FormControl('', [Validators.required]),
+    topicsControl: new FormControl('')
   });
+  @ViewChild('topicInput') topicInput!: ElementRef<HTMLInputElement>;
+  topics: Topic[] = [];
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  allTopics: Topic[] = [];
+  filteredTopics!: Observable<Topic[]>;
+
 
   group!: Group;
+  displayGroup!: {
+    id: string,
+    name: string,
+    description: string,
+    location: string,
+    topicNames: string[]
+  };
   constructor(
     private groupService: GroupsService,
     private dialogRef: MatDialogRef<EditGroupDialogComponent>,
@@ -30,18 +49,49 @@ export class EditGroupDialogComponent implements OnInit, OnDestroy {
   ) {
     if(data.group) {
       this.group = data.group;
-      this.formGroup.patchValue(this.group);
+      this.displayGroup = {
+        id: this.group.id,
+        name: this.group.name,
+        description: this.group.description,
+        location: this.group.location,
+        topicNames: this.group.topics.map(topic => topic.name)
+      }
+      this.formGroup.patchValue(this.displayGroup);
+      this.topics = [...this.group.topics];
+      this.filteredTopics = this.formGroup.get('topicsControl')!.valueChanges.pipe(
+        startWith(null),
+        map((topic: string | null) => topic ? this._filter (topic) : this.allTopics.slice())
+      );
     }
   }
 
   ngOnInit(): void {
+    this.groupService.getTopics().subscribe(
+      (response: Topic[]) => {
+        this.allTopics = response;
+      }, (errorResponse: HttpErrorResponse) => {
+        console.log(errorResponse.error);
+        this.sendNotification(NotificationType.ERROR, errorResponse.error.message);
+      }
+    );
   }
 
   ngOnDestroy(): void {
+    console.log(this.topics);
+    console.log("Group obj Topics ");
+    console.log(this.group.topics);
+    this.topics = [];
   }
 
   editGroup() {
-    let group: Group = this.formGroup.value;
+    let group: Group = {
+      id: this.formGroup.get('id')!.value,
+      name: this.formGroup.get('name')!.value,
+      description: this.formGroup.get('description')!.value,
+      location: this.formGroup.get('location')!.value,
+      nextMeetingDate: new Date(),
+      topics: this.topics
+    }
     this.groupService.updateGroup(group).subscribe(
       (response: Group) => {
         console.log(response);
@@ -60,5 +110,43 @@ export class EditGroupDialogComponent implements OnInit, OnDestroy {
     } else {
       this.notificationService.notify(notificationType, "An error occurred. Please try again");
     }
+  }
+
+  addTopic(event: MatChipInputEvent) {
+    const input = event.chipInput;
+    const value = (event.value || '').trim();
+
+    if (value) {
+      let topic: Topic = {
+        id: '',
+        name: value,
+        description: ''
+      }
+      this.topics.push(topic);
+    }
+    input!.clear();
+    this.formGroup.get('topicsControl')?.setValue(null);
+  }
+
+  removeTopic(topic: Topic) {
+    const index = this.topics.indexOf(topic);
+    if (index >= 0) {
+      this.topics.splice(index, 1);
+    }
+  }
+
+  selected($event: MatAutocompleteSelectedEvent) {
+    this.topics.push($event.option.value);
+    this.topicInput.nativeElement.value = '';
+    this.formGroup.get('topicsControl')?.setValue(null);
+  }
+
+  private _filter(value: string): Topic[] {
+    const filterValue = value.toLowerCase();
+    return this.allTopics.filter(topic => topic.name.toLowerCase().includes(filterValue));
+  }
+
+  capitalize(str: string): string {
+    return str.replace(/\b\w/g, (char: string) => char.toUpperCase());
   }
 }
